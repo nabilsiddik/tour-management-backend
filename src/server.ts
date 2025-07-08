783043afbdfcbe19f75f798dbc2a2be502f86575
nabilsiddik <nabilsiddik90@gmail.com>
2025-07-08 13:09:41 +0600

    test commit

---
+++ src/server.ts
@@ -0,0 +1,73 @@
+import { Server } from "http";
+let server: Server;
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./app";
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+// Start server
+const startServer = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}`);
+    console.log("Mongodb connected");
+
+    server = app.listen(port, () => {
+      console.log(`Tour Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Error while connecting database server");
+  }
+};
+
+startServer();
+
+// Unhandled rejection error
+process.on("unhandledRejection", (err)=> {
+    console.log("Unhandled Rejection detected. Server shutting down ...", err)
+
+    if(server){
+        server.close(()=>{
+            process.exit()
+        })
+    }
+    process.exit()
+})
+
+// Uncaught exception error
+process.on("uncaughtException", (err)=> {
+    console.log("uncaught Exception detected. Server shutting down ...", err)
+
+    if(server){
+        server.close(()=>{
+            process.exit()
+        })
+    }
+    process.exit()
+})
+
+// Signal termination error
+process.on("SIGTERM", (err)=> {
+    console.log("Sigterm signal received... Server shutting down ...", err)
+
+    if(server){
+        server.close(()=>{
+            process.exit()
+        })
+    }
+    process.exit()
+})
+
+process.on("SIGINT", (err)=> {
+    console.log("SIGINT signal received... Server shutting down ...", err)
+
+    if(server){
+        server.close(()=>{
+            process.exit()
+        })
+    }
+    process.exit()
+})
+
+// Promise.reject(new Error("I forgot to catch this promise"))
+// throw new Error('I forgot to handle this local error')
