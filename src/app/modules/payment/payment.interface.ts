474d00b0d89b087b862d5168894e0fbcfd3ae748
nabilsiddik <nabilsiddik90@gmail.com>
2025-07-22 21:19:13 +0600

    working with booking models

---
+++ src/app/modules/payment/payment.interface.ts
@@ -0,0 +1,18 @@
+import { Types } from "mongoose";
+
+export enum PAYPMENT_STATUS{
+    PAID = 'PAID',
+    UNPAID = 'UNPAID',
+    FAILED = 'FAILED',
+    CANCLED = 'CANCLED',
+    REFUNDED = 'REFUNDED'
+}
+
+export interface IPayment{
+    booking: Types.ObjectId,
+    transactionId: string,
+    amount: number,
+    paymentGetway?: any,
+    invoice?: string,
+    status: PAYPMENT_STATUS
+}
---
+++ src/app/modules/payment/payment.model.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from "mongoose";
+import { IPayment, PAYPMENT_STATUS } from "./payment.interface";
+
+const paymentSchema = new Schema<IPayment>({
+    booking: {
+        type: Schema.Types.ObjectId,
+        ref: "Booking",
+        required: true,
+        unique: true,
+    },
+    transactionId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    status: {
+        type: String,
+        enum: Object.values(PAYPMENT_STATUS),
+        default: PAYPMENT_STATUS.UNPAID
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    paymentGetway: {type: Schema.Types.Mixed},
+    invoice: {
+        type: String
+    }
+}, {
+    timestamps: true,
+    versionKey: false
+})
+
+const Booking = model<IPayment>("Payment")
+
+export default Booking
